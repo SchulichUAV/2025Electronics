47e1bf3e5a43a0e81b84dc50a23cf17accc3f697
Liam <liam.d.mah@gmail.com>
2025-05-24 21:28:05 -0600

    Updating the autopilotdevelopment submodule to 'comp' branch which has the raspberry pi import statements working for it.
    Also removing the planeObject and copterObject since this is breaking on the Pi.

--- server.py
+++ server.py
@@ -17,9 +17,6 @@ from os import path
 import argparse
 import RPi.GPIO as GPIO
 
-from modules.AutopilotDevelopment.Plane.planeObject import Plane
-from modules.AutopilotDevelopment.Copter.copterObject import Copter
-
 import modules.AutopilotDevelopment.General.Operations.initialize as initialize
 import modules.AutopilotDevelopment.General.Operations.mode as autopilot_mode
 import modules.AutopilotDevelopment.General.Operations.mission as mission
@@ -283,15 +280,7 @@ def receive_vehicle_position():  # Actively runs and receives live vehicle data 
             print(f"Received data item does not match expected length...")
 
 if __name__ == "__main__":
-    # Need to take a parameter off of the command line to determine if we are a plane or copter
-    if(sys.argv[1].lower() == "plane"):
-        vehicle = Plane()
-    elif(sys.argv[1].lower() == "copter"):
-        vehicle = Copter() 
-    else:
-        vehicle = Plane()
-        print("Defaulting into plane.")
-    
+    # Need to take a parameter off of the command line to determine if we are a plane or copter 
     kit = ServoKit(channels=16)
     current_available_servo = 0
 
